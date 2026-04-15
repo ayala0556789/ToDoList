5e65f896a4cf685f4943814cf245d9ddd22d1580
ayala <ayala0556789@gmail.com>
2026-04-15 19:30:26 +0300

    update api url to render

--- client/src/service.js
+++ client/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL= "http://localhost:5058"
-
+// axios.defaults.baseURL= "http://localhost:5058"
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 // --- : הוספת טוקן לכל בקשה יוצאת ---
 axios.interceptors.request.use(config => {
